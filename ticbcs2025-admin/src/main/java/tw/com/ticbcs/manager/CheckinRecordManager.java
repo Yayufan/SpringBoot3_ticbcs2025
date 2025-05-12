37209e6e2aadfe12140de91adf3b9fd486743ad2
Yayufan <kamikazey2200@gmail.com>
2025-05-12 17:20:14 +0800

    Update CheckinRecordManager.java

--- ticbcs2025-admin/src/main/java/tw/com/ticbcs/manager/CheckinRecordManager.java
+++ ticbcs2025-admin/src/main/java/tw/com/ticbcs/manager/CheckinRecordManager.java
@@ -17,35 +17,33 @@ import tw.com.ticbcs.pojo.entity.CheckinRecord;
 public class CheckinRecordManager {
 
 	private final CheckinRecordMapper checkinRecordMapper;
-	
-	public CheckinInfoBO getLastCheckinRecordByAttendeesId (Long attendeesId) {
+
+	public CheckinInfoBO getLastCheckinRecordByAttendeesId(Long attendeesId) {
 		// 先找到這個與會者所有的checkin紀錄
 		LambdaQueryWrapper<CheckinRecord> checkinRecordWrapper = new LambdaQueryWrapper<>();
-		checkinRecordWrapper.eq(CheckinRecord::getAttendeesId,attendeesId);
+		checkinRecordWrapper.eq(CheckinRecord::getAttendeesId, attendeesId);
 		List<CheckinRecord> checkinRecordList = checkinRecordMapper.selectList(checkinRecordWrapper);
-		
+
 		CheckinInfoBO checkinInfoBO = new CheckinInfoBO();
 		LocalDateTime checkinTime = null;
 		LocalDateTime checkoutTime = null;
 
 		for (CheckinRecord record : checkinRecordList) {
-		    if (record.getActionType() == 1) {
-		        if (checkinTime == null || record.getActionTime().isBefore(checkinTime)) {
-		            checkinTime = record.getActionTime();
-		        }
-		    } else if (record.getActionType() == 2) {
-		        if (checkoutTime == null || record.getActionTime().isAfter(checkoutTime)) {
-		            checkoutTime = record.getActionTime();
-		        }
-		    }
+			if (record.getActionType() == 1) {
+				if (checkinTime == null || record.getActionTime().isBefore(checkinTime)) {
+					checkinTime = record.getActionTime();
+				}
+			} else if (record.getActionType() == 2) {
+				if (checkoutTime == null || record.getActionTime().isAfter(checkoutTime)) {
+					checkoutTime = record.getActionTime();
+				}
+			}
 		}
 
 		checkinInfoBO.setCheckinTime(checkinTime);
 		checkinInfoBO.setCheckoutTime(checkoutTime);
-		
-		
+
 		return checkinInfoBO;
 	}
-	
-	
+
 }
